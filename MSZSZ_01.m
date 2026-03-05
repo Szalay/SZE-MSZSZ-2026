6ca0171e7f552892ffd8c03350a542abee82dea9
Szalay <ifj.szalay.istvan@gmail.com>
2026-03-05 11:39:28 +0100

    Az első héten felírt gondolatok.

---
+++ MSZSZ_01.m
@@ -0,0 +1,18 @@
+% Elnevezési íratlan szabályok
+% Változók: camelCase
+% Függvények: PascalCase
+
+% Normálalak: 5,6·10^-3
+
+% Műveleti sorrend (operator precedence)
+
+% Függvényhívás:
+% visszatérési érték(ek) = függvénynév(bemenet(ek))
+
+
+% A MATLAB numerikus rendszer
+% - véges számú bit => numerikus hiba
+
+% Derékszögű háromszög átfogója
+% a^2 + b^2 = c^2
+% c = sqrt(a^2 + b^2)
