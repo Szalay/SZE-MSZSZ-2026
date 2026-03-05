e1d1e4ef42787df90fd38bf1357a89829571258a
Szalay <ifj.szalay.istvan@gmail.com>
2026-03-05 14:29:07 +0100

    A Tools.Figure-nak lehet nevet megadni.

--- Tools.m
+++ Tools.m
@@ -2,8 +2,11 @@ classdef Tools
 	
 	methods (Static)
 		
-		function window = Figure()
-			window = figure();
+		function window = Figure(Settings)
+			arguments
+				Settings.Name(1, 1) string = ""
+			end
+			window = figure(Name=Settings.Name);
 			window.Color = [1, 1, 1];
 			hold on; box on; grid on;
 		end
