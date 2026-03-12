b4f893c4997bef9339bf89cbcbb7aafa100fe4d5
Szalay <ifj.szalay.istvan@gmail.com>
2026-03-12 12:39:15 +0100

    A tekercsmodell teljesítményei.
    A tekercsmodellt kiegészítettük a teljesítmények számításával és ábrázolásával.

--- Coil.m
+++ Coil.m
@@ -2,6 +2,10 @@ classdef Coil < handle
 	%COIL Tekercsmodell
 	%   Lineáris mágnesezésű tekercsmodell
 	%   u_BE(t) = R i(t) + L di/dt
+	%   P(t) = u_BE(t) i(t)
+	%   P_R(t) = u_R(t) i_R(t) = R i^2(t)
+	%   P_L(t) = u_L(t) i_L(t) = L i(t) di/dt
+	%   P(t) ?= P_R(t) + P_L(t)
 	
 	% Tulajdonságok
 	properties
@@ -18,6 +22,11 @@ classdef Coil < handle
 		% Megoldás (oszlopvektorok)
 		t(:, 1);		% Idővektor
 		i(:, 1);		% Az áramerősség
+		
+		% Teljesítmények
+		P(:, 1);		% A felvett/bejövő villamos teljesítmény
+		P_R(:, 1);		% Az ellenállás teljesítményvesztesége
+		P_L(:, 1);		% Az induktivitás teljesítményfelvétele
 	end
 	
 	% Tagfüggvények
@@ -29,7 +38,7 @@ classdef Coil < handle
 				Settings.R(1, 1) {mustBePositive}
 				Settings.L(1, 1) {mustBePositive}
 				Settings.i_0(1, 1) = 0
-				Settings.u_BE(1, 1) function_handle = @(t)0;
+				Settings.u_BE(1, 1) function_handle = @(t)zeros(size(t));
 				Settings.t(:, 1)
 			end
 			
@@ -53,12 +62,31 @@ classdef Coil < handle
 			[this.t, this.i] = ode45(@this.Model, this.t, this.i_0);
 		end
 		
+		% Utófeldolgozás
+		function PostProcessing(this, Settings)
+			arguments
+				this
+				Settings.checkPowerEquality(1, 1) logical = false;
+			end
+			this.P = this.u_BE(this.t) .* this.i;
+			this.P_R = this.R * this.i.^2; % this.i .* this.i
+			
+			didt = this.Model(this.t, this.i);
+			this.P_L = this.L * this.i .* didt;
+			
+			if Settings.checkPowerEquality
+				D = this.P - (this.P_R + this.P_L);
+				S = sum(D.^2);
+				disp("A teljesítményszámítás hibanégyzetösszege: " + S);
+			end
+		end
+		
 		% Ábrázolás
 		function Plot(this)
 			Tools.Figure(Name="Tekercs válaszárama");
 			
 			% Feszültség
-			s_u = subplot(2, 1, 1);
+			s_u = subplot(3, 1, 1);
 			hold on; box on; grid on;
 			
 			title("Bemeneti feszültség", FontSize=18);
@@ -70,7 +98,7 @@ classdef Coil < handle
 			legend(p_u, "{\itu}_{BE}({\itt})", FontSize=14);
 			
 			% Áramerősség
-			s_i = subplot(2, 1, 2);
+			s_i = subplot(3, 1, 2);
 			hold on; box on; grid on;
 			
 			title("Kimeneti áramerősség", FontSize=18);
@@ -81,7 +109,32 @@ classdef Coil < handle
 			
 			legend(p_i, "{\iti}({\itt})", FontSize=14);
 			
-			linkaxes([s_u, s_i], "x");
+			% Teljesítmények
+			s_p = subplot(3, 1, 3);
+			hold on; box on; grid on;
+			
+			title("A tekercshez kapcsolódó teljesítmények", FontSize=18);
+			xlabel("Idő, {\itt}, (s)", FontSize=16);
+			ylabel("Teljesítmény, {\itP}, (W)", FontSize=16);
+			
+			a_rl = area(this.t, [this.P_R, this.P_L]);
+			
+			a_rl(1).FaceColor = "y";
+			a_rl(2).FaceColor = "c";
+			a_rl(2).FaceAlpha = 0.5;
+			
+			p_p = plot(this.t, this.P, "k", LineWidth=3);
+			%p_pr = plot(this.t, this.P_R, "y", LineWidth=3);
+			%p_pl = plot(this.t, this.P_L, "m", LineWidth=3);
+			
+			legend( ...
+				[p_p, a_rl(1), a_rl(2)], ...
+				["{\itP}({\itt})", "{\itP_R}({\itt})", "{\itP_L}({\itt})"], ...
+				FontSize=14 ...
+				);
+			
+			% A vízszintes nagyítás összekapcsolása
+			linkaxes([s_u, s_i, s_p], "x");
 		end
 		
 	end
@@ -91,11 +144,12 @@ classdef Coil < handle
 		
 		function Run()
 			tekercs = Coil( ...
-				R=1, L=10e-3, i_0=-200e-3, ...
-				u_BE=@(t)5*cos(2*pi*500*t), ...
-				t=0:20e-6:50e-3 ...
+				R=5, L=10e-3, i_0=-200e-3, ...
+				u_BE=@(t)5*cos(2*pi*250*t), ...
+				t=0:20e-6:20e-3 ...
 				);
 			tekercs.Simulate();
+			tekercs.PostProcessing(checkPowerEquality=true);
 			tekercs.Plot();
 		end
 		
